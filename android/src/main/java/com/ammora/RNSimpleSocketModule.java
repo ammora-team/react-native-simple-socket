a7abce906cec939b1d98af50e86e07c5f78443ec
ramonornela <ramon.ornela@gmail.com>
2019-05-31 20:52:40 -0300

    refactor(module): changes callback to promise

--- android/src/main/java/com/ammora/RNSimpleSocketModule.java
+++ android/src/main/java/com/ammora/RNSimpleSocketModule.java
@@ -4,90 +4,38 @@ package com.ammora;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
-import com.facebook.react.bridge.GuardedAsyncTask;
-import android.os.AsyncTask;
-import java.util.ArrayDeque;
-import java.util.HashSet;
-import java.util.concurrent.Executor;
+import com.facebook.react.bridge.Promise;
 import java.net.*;
 import java.io.*;
 
 public class RNSimpleSocketModule extends ReactContextBaseJavaModule {
-
-  private final ReactApplicationContext reactContext;
-
-  private class SerialExecutor implements Executor {
-    private final ArrayDeque<Runnable> mTasks = new ArrayDeque<Runnable>();
-    private Runnable mActive;
-    private final Executor executor;
-
-    SerialExecutor(Executor executor) {
-      this.executor = executor;
-    }
-
-    public synchronized void execute(final Runnable r) {
-      mTasks.offer(new Runnable() {
-        public void run() {
-          try {
-            r.run();
-          } finally {
-            scheduleNext();
-          }
-        }
-      });
-      if (mActive == null) {
-        scheduleNext();
-      }
-    }
-    synchronized void scheduleNext() {
-      if ((mActive = mTasks.poll()) != null) {
-        executor.execute(mActive);
-      }
-    }
-  }
-
-  private final SerialExecutor executor;
-
   public RNSimpleSocketModule(ReactApplicationContext reactContext) {
-    this(reactContext, AsyncTask.THREAD_POOL_EXECUTOR);
-  }
-
-  public RNSimpleSocketModule(ReactApplicationContext reactContext, Executor executor) {
     super(reactContext);
-    this.reactContext = reactContext;
-    this.executor = new SerialExecutor(executor);
   }
 
   @ReactMethod
-  public void send(final String hostname, final int port, final String data, final Callback callback) {
-    new GuardedAsyncTask<Void, Void>(getReactApplicationContext()) {
-      @Override
-      protected void doInBackgroundGuarded(Void... params) {
-        try (Socket socket = new Socket(hostname, port)) {
+  public void send(final String hostname, final int port, final String data, final Promise promise) {
+    try (Socket socket = new Socket(hostname, port)) {
+      OutputStream output = socket.getOutputStream();
+      PrintWriter writer = new PrintWriter(output, true);
+      writer.println(data);
 
-          OutputStream output = socket.getOutputStream();
-          PrintWriter writer = new PrintWriter(output, true);
-          writer.println(data);
+      InputStream input = socket.getInputStream();
 
-          InputStream input = socket.getInputStream();
+      BufferedReader reader = new BufferedReader(new InputStreamReader(input));
 
-          BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+      String line;
+      String result = "";
 
-          String line;
-          String result = "";
-
-          while ((line = reader.readLine()) != null) {
-            result = result.concat(line);
-          }
-          callback.invoke(null, result);
-        } catch (UnknownHostException ex) {
-          callback.invoke(ex, null);
-        } catch (IOException ex) {
-          callback.invoke(ex, null);
-        }
+      while ((line = reader.readLine()) != null) {
+        result = result.concat(line);
       }
-    }.executeOnExecutor(executor);
+      Promise.resolve(result);
+    } catch (UnknownHostException ex) {
+      Promise.reject(ex);
+    } catch (IOException ex) {
+      Promise.reject(ex);
+    }
   }
 
   @Override
--- index.js
+++ index.js
@@ -1,31 +1,3 @@
 import { NativeModules } from 'react-native';
 
-const { RNSimpleSocket } = NativeModules;
-
-const Socket = {
-  send: function(hostname, port, data) {
-    return new Promise((resolve, reject) => {
-      RNSimpleSocket.send(hostname, port, data, function(error, result) {
-        const err = convertError(error);
-        const value = result ? result : null;
-        if (err) {
-          reject(err);
-        } else {
-          resolve(value);
-        }
-      });
-    });
-  }
-}
-
-function convertError(error) {
-  if (!error) {
-    return null;
-  }
-  const out = new Error(error.message);
-  // $FlowFixMe: adding custom properties to error.
-  out.key = error.key;
-  return out;
-}
-
-export default Socket
+export default NativeModules.RNSimpleSocket;
